1861559ae13e31ca62542a38a843977a2e879ca8
Sojome <sojome@outlook.com>
2019-05-20 16:10:04 -0500

    Initial commit

---
+++ ServletEjemplo/src/ejemplo/hola.java
@@ -0,0 +1,30 @@
+package ejemplo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class hola
+ */
+@WebServlet("/hola")
+public class hola extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public hola() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hola personas");
+	}
+
+}
